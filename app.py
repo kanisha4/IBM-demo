4a8093d8ba10718bb2e01de91233543c9656e6f5
kanisha4 <48114734+kanisha4@users.noreply.github.com>
2021-02-03 00:34:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_table
+from dash.dependencies import Input, Output
+import plotly.express as px
+import plotly.graph_objs as go   #for scatter graph
+import pandas as pd
+
+data = pd.read_csv("dth_data_1.csv")
+
+ott_subs = dict(data.ott_subscription.value_counts())
+#print(data.ott_subscription.value_counts())
+
+fig = px.bar(x=list(ott_subs.keys()), y=list(ott_subs.values()), title="Popularity of OTT Platform ")
+
+app = dash.Dash()
+server = app.server  
+
+
+app.layout = html.Div([
+    dcc.Graph(id = "bar-graph", figure = fig)
+    ])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
